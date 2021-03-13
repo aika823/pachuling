47401e42037d4321314b27e36b157737c587f804
Choi-In-Kyu <aika823@naver.com>
2021-03-13 13:27:48 +0900

    일부 기능 수정

--- application.py
+++ application.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, request, json, flash, redirect, url_for
+from flask import Flask, render_template, request
 import db as db
-import forms
+import call_fuction as call_function
+import json
+from flask.json import JSONEncoder
+from datetime import date
 
-# import call_fuction as call_function
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.strftime('%Y-%m-%d')
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
 
 application = Flask(__name__)
-page_list = {'call': None, 'company': None, 'employee': None}
+application.json_encoder = CustomJSONEncoder
+page_list = {'call': None, 'company': None, 'employee': None, 'manage': None}
 
 
 def select_page(page):
@@ -18,8 +34,12 @@ def select_page(page):
 def call():
     select_page('call')
     calls = db.get_calls()
-    test = "test99999999999"
-    return render_template('call/call.html', calls=calls, page_list=page_list, test=test)
+    print("###########")
+    print(calls)
+    print(len(calls))
+    print("###########")
+    call_dict = call_function.calculate_price(calls)
+    return render_template('call/call.html', calls=calls, call_dict=call_dict, page_list=page_list)
 
 
 @application.route('/call', methods=['POST'])
@@ -30,9 +50,15 @@ def search_call():
         end = request.form['end']
         content = request.form['content']
         calls = db.get_calls(start, end, content)
-        # if len(calls) > 0:
-        #     calls = call_function.search_mark(calls, content)
-        return render_template('call/call.html', calls=calls, start=start, end=end, content=content, page_list=page_list)
+        if len(calls) > 0:
+            calls = call_function.search_mark(calls, content)
+        return render_template('call/call.html',
+                               calls=calls, start=start, end=end, content=content, page_list=page_list)
+
+
+@application.route('/call/write')
+def call_form():
+    return render_template('call/callForm.html', page_list=page_list)
 
 
 @application.route('/company')
@@ -42,31 +68,35 @@ def company():
     return render_template('company/company.html', companies=companies, page_list=page_list)
 
 
+@application.route('/company/write')
+def company_form():
+    return render_template('company/companyForm.html', page_list=page_list)
+
+
 @application.route('/employee')
 def employee():
     select_page('employee')
-    return render_template('employee.html', page_list=page_list)
+    # employees = db.get_employees()
+    return render_template('employee/employee.html', page_list=page_list)
 
 
-@application.route('/ceo/<id>')
-def show_ceo(id):
-    return render_template('ceo/home.html', id=id)
+@application.route('/employeeForm')
+def employee_form():
+    select_page('employeeForm')
+    return render_template('employee/employeeForm.html', page_list=page_list)
 
 
-@application.route('/login')
-def login():
-    form = forms.RegistrationForm()
-    return render_template('login/login.html', form=form)
+@application.route('/employeeAvailable')
+def employee_available():
+    select_page('employeeAvailable')
+    return render_template('employee/employeeAvailable.html', page_list=page_list)
 
 
-@application.route('/login', methods=['POST'])
-def register():
-    form = forms.RegistrationForm()
-    if form.validate_on_submit():
-        # 알람 카테고리에 따라 부트스트랩에서 다른 스타일을 적용 (success, danger)
-        flash(f'{form.username.data} 님 가입 완료!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', form=form)
+@application.route('/manage')
+def manage():
+    select_page('black')
+    companies = db.get_companies()
+    return render_template('manage/black.html', companies=companies, page_list=page_list)
 
 
 if __name__ == "__main__":
