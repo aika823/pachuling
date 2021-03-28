9f8a8b2cc5bfe567e83ee490ae2ed5aefde47df3
Choi-In-Kyu <aika823@naver.com>
2021-03-28 12:55:56 +0900

    0328 로그인 기능 임시 배제

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import database_function
 import call_function
 import company_function
+# 이 부분에서 배포 문제 발생
 # import database_class
 
 import secret
@@ -24,20 +25,6 @@ def select_page(page):
 
 
 @application.route('/')
-def fuck():
-    calls = database_function.get_calls()
-    call_dict = call_function.calculate_price(calls)
-    return render_template('call/call.html', calls=calls, call_dict=call_dict, page_list=page_list)
-
-
-#     # if not session.get('logged_in'):
-#     #     return redirect(url_for('login'))
-#     # else:
-#     #     select_page('call')
-#     #     calls = database_function.get_calls()
-#     #     call_dict = call_function.calculate_price(calls)
-#     #     return render_template('call/call.html', calls=calls, call_dict=call_dict, page_list=page_list)
-
 @application.route('/call')
 def call():
     calls = database_function.get_calls()
@@ -71,17 +58,17 @@ def search_call():
 
 @application.route('/call/write')
 def call_form():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         return render_template('call/callForm.html', page_list=page_list)
 
 
 @application.route('/company')
 def company():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         select_page('company')
         companies = database_function.get_companies()
         return render_template('company/company.html', companies=companies, page_list=page_list)
@@ -101,9 +88,9 @@ def search_company():
 
 @application.route('/company/view/<company_id>')
 def view_company(company_id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         select_page('company')
         my_company = database_function.get_company(company_id)
         return render_template('company/view.html', company=my_company, page_list=page_list)
@@ -111,17 +98,17 @@ def view_company(company_id):
 
 @application.route('/company/write')
 def company_form():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         return render_template('company/companyForm.html', page_list=page_list)
 
 
 @application.route('/employee')
 def employee():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         select_page('employee')
         employees = database_function.get_employees()
         return render_template('employee/employee.html', employees=employees, page_list=page_list)
@@ -129,27 +116,27 @@ def employee():
 
 @application.route('/employee/write')
 def employee_form():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         select_page('employeeForm')
         return render_template('employee/employeeForm.html', page_list=page_list)
 
 
 @application.route('/employee/available')
 def employee_available():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         select_page('employeeAvailable')
         return render_template('employee/employeeAvailable.html', page_list=page_list)
 
 
 @application.route('/manage')
 def manage():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         select_page('black')
         companies = database_function.get_companies()
         return render_template('manage/black.html', companies=companies, page_list=page_list)
@@ -157,9 +144,9 @@ def manage():
 
 @application.route('/ceo/<ceo_id>')
 def show_ceo(ceo_id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
+    # if not session.get('logged_in'):
+    #     return redirect(url_for('login'))
+    # else:
         ceo = database_function.get_ceo(ceo_id)
         return render_template('ceo/ceo.html', ceo=ceo, page_list=page_list)
 
